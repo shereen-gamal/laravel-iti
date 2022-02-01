a86ea63d7dce69df468d21cde5a8f00d7cd58c6b
shereen_28 <gmshereen98@gmail.com>
2022-02-01 15:01:40 +0200

    auth:sanctum commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +23,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts',[PostController::class,'index']);
-Route::get('/posts/{post}',[PostController::class,'show']);
-Route::post('/posts',[PostController::class, 'store']);
+Route::get('/posts',[PostController::class,'index'])->middleware('auth:sanctum');
+Route::get('/posts/{post}',[PostController::class,'show'])->middleware('auth:sanctum');
+Route::post('/posts',[PostController::class, 'store'])->middleware('auth:sanctum');
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
 
